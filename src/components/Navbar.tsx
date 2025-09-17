ef494a3d65d20a84adb36f4d2e9d966e62e96712
Ivan Ventura Segui <ivan.ventura@alumnos.upm.es>
2025-09-17 19:35:51 +0200

    bug css arreglado, colores y fuentes añadidas, navbar mejorada

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,27 @@
 import Link from "next/link";
+import '@/app/globals.css';
+
 
 export default function Navbar() {
   return (
-    <nav className="bg-white shadow p-4">
-      <ul className="flex gap-6">
-        <li><Link href="/">Home</Link></li>
-        <li><Link href="/about">About</Link></li>
-        <li><Link href="/projects">Projects</Link></li>
-        <li><Link href="/contact">Contact</Link></li>
-      </ul>
+    <nav className="navbar-fija">
+      <div className="flex flex-row items-center justify-between margen-left-med">
+        {/* Nombre a la izquierda */}
+        <div id="nombre">
+          <Link href="/" className="text-xl font-bold text-gray-900">
+            Iván Ventura
+          </Link>
+        </div>
+        {/* Secciones alineadas a la derecha */}
+        <div id="secciones">
+          <ul className="flex flex-row">
+            <li className="margen-right-med"><Link href="/">Home</Link></li>
+            <li className="margen-right-med"><Link href="/about">About</Link></li>
+            <li className="margen-right-med"><Link href="/projects">Projects</Link></li>
+            <li className="margen-right-med"><Link href="/contact">Contact</Link></li>
+          </ul>
+        </div>
+      </div>
     </nav>
   )
 }
