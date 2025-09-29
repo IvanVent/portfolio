ad1fe4b241ae91f101d7cb2bb9ea59e69edf1f47
Ivan Ventura Segui <ivan.ventura@alumnos.upm.es>
2025-09-29 18:44:12 +0200

    estilos about creados en css y comienzo a adaptar web a formato responsive

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,14 +3,15 @@ import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import I18nProvider from "../components/I18nProvider";
 import './globals.css'
+import '../styles/mobile.css'
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="es">
-      <body className="bg-gray-50 text-gray-900 wrapper">
+      <body className="wrapper">
         <I18nProvider>
           <Navbar />
-          <main className="min-h-screen p-6">{children}</main>
+          <main>{children}</main>
           <Footer />
         </I18nProvider>
         <br />
